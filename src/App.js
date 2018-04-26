712f0eaed42102925b51a006a7c07225f5093eb7
Lenar Mukhamadiev <baltachtan@yahoo.com>
2018-04-26 14:40:16 -0400

    API, styling, UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 const PLACES = [
   { name: "Palo Alto", zip: "94303"},
-  { name: "Pittsburgh", zip: "15213"},
+  { name: "Pittsburgh", zip: "15106"},
   { name: "Chicago", zip: "60607"},
   { name: "Honolulu", zip: "96803"}
 ];
@@ -51,6 +51,7 @@ class WeatherDisplay extends Component {
   }
   componentDidMount() {
     const zip = this.props.zip;
+    console.log(zip);
     const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
     zip +
       "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
@@ -61,6 +62,20 @@ class WeatherDisplay extends Component {
   render() {
     const weatherData = this.state.weatherData;
     if (!weatherData) return <div>Loading</div>
+    const weather = weatherData.weather[0];
+    const iconURL = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconURL} alt = {weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°F</p>
+        <p>High: {weatherData.main.temp_max}°F</p>
+        <p>Low: {weatherData.main.temp_min}°F</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    )
     return <div>{JSON.stringify}</div>
   }
 }
